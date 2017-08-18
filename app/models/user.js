02ba48548bfa9de48b364c523ee44f85d5f2f627
Vasu Jogani <vasujogani@Vasus-MBP.austin.rr.com>
2017-08-18 00:35:30 -0500

    added encrypting

--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
 
 var UserSchema = new Schema({
   username:{type:String, lowercase: true, unique: true, required: true},
@@ -7,4 +8,12 @@ var UserSchema = new Schema({
   email:{type:String, required:true, lowercase: true, unique: true}
 });
 
+UserSchema.pre('save', function(next){
+  var user = this;
+  bcrypt.hash(user.password, null, null, function(err, hash){
+    if(err) return next(err);
+    user.password = hash;
+    next();
+  });
+});
 module.exports = mongoose.model('User', UserSchema);
